664e1df357bfb4b2f3fa3da43b30fcb86e356afa
StackMastery <labib.developers@gmail.com>
2025-01-15 13:35:02 -0500

    Donor Dashboard recent donation api done

--- routes/Routes.js
+++ routes/Routes.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import VerifyToken from '../middlewares/verifyToken.js'
 import CreateBloodDonation from '../controllers/BloodDonatin/CreateBloodDonation.js'
+import { GetBloodDonationForDonor } from '../controllers/BloodDonatin/GetBloodDonation.js'
 
 const Routes = express.Router()
 
 //-------- Donation Crud
 // Create Donation
 Routes.post(`/donation/create`, VerifyToken, CreateBloodDonation)
+// Get Blood donation Request with limit
+Routes.get(`/donation`, VerifyToken, GetBloodDonationForDonor)
 
 
 export { Routes }
