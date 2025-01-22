88b8aeecb672820e6f7454bde8d662e4df3c2dd9
StackMastery <labib.developers@gmail.com>
2025-01-21 23:53:06 -0500

    Blog page api done

--- controllers/frontend/GetBlogPostFrontend.js
+++ controllers/frontend/GetBlogPostFrontend.js
@@ -1,12 +1,32 @@
 import BlogModel from '../../models/blog.model.js'
 
 const GetBlogPostFrontend = async (req, res) => {
-    try{
-        const getBlogs = await BlogModel.find({status: 'published'}).select('-content').sort({createdAt: -1}).limit(req.query.limit || 3)
-        res.status(200).send(getBlogs);
-    }
-    catch(err){
-        res.status(500).send({ error: 'An error occurred while counting' });
+    const { search, limit } = req.query
+
+    try {
+        const searchQuery = search ? { 
+            $or: [
+                { postTitle: { $regex: search, $options: 'i' } },
+                { shortDescription: { $regex: search, $options: 'i' } },
+            ] 
+        } : {}
+
+        const getBlogs = await BlogModel.find({ 
+            ...searchQuery, 
+            status: 'published' 
+        }).select('-content').sort({ createdAt: -1 }).limit(Number(limit) || 3)
+
+        const blogsCountTotal = await BlogModel.countDocuments({
+            ...searchQuery,
+            status: 'published'
+        })
+
+        res.status(200).send({
+            blogsCount: blogsCountTotal,
+            blogs: getBlogs,
+        })
+    } catch (err) {
+        res.status(500).send({ error: 'An error occurred while retrieving the blogs' })
     }
 }
 
